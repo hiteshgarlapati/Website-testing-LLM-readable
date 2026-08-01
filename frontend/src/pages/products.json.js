fc84839c8974adc9937c05deeb16617807b671c2
tantechmek-create <tan.tech.mek@gmail.com>
2026-08-01 14:52:33 +0530

    Front_end

--- frontend/src/pages/products.json.js
+++ frontend/src/pages/products.json.js
@@ -1,8 +1,7 @@
-import { getAllItems } from '../lib/db.js';
+import itemsData from '../data/items.json';
 
 export async function GET() {
-  const items = await getAllItems();
-  return new Response(JSON.stringify(items, null, 2), {
+  return new Response(JSON.stringify(itemsData, null, 2), {
     headers: { 'content-type': 'application/json; charset=utf-8' }
   });
 }
